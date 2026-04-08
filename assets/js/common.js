c2b081ceb85c2e36942e8f31827ea8609b76afc9
Jett <cresumer900317@gmail.com>
2026-04-08 18:54:48 +0900

    feat: 길드 내전 페이지 추가 (친구둘 vs 친구삼 vs 친구넷)

--- assets/js/common.js
+++ assets/js/common.js
@@ -118,6 +118,7 @@ function renderShell() {
     ${navLink("./members.html", "members", "길드원", page)}
     ${navLink("./weekly.html", "weekly", "월간성장", page)}
     ${navLink("./rivals.html", "rivals", "⚔️ 라이벌전", page)}
+    ${navLink("./civil.html", "civil", "🛡️ 내전", page)}
     ${navLink("./notice.html", "notice", "공지", page)}
     ${navLink("./tips.html", "tips", "팁", page)}
   `;
